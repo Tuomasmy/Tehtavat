73742ffcce4da294ae39d42e6d7795054cf408dc
Tuomasmy <36227446+Tuomasmy@users.noreply.github.com>
2019-02-13 09:07:57 +0200

    Delete js1.js

--- parallax/js1.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-function myFunction(e) {
-  let x = e.clientX;
-  let y = e.clientY;
-  let coor = "Coordinates: (" + x + "," + y + ")";
-  document.getElementById("coord").innerHTML = coor;
-}
